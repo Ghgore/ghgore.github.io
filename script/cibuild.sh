c0d90376015f45773660d92d0d48e74ea3d8fc50
Griffin Gore <griffin.gore@outlook.com>
2019-10-10 11:12:29 -0400

    Updated File extensions

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -5,5 +5,5 @@ set -e
 bundle exec jekyll build
 bundle exec htmlproofer ./_site --disable-external
 bundle exec rubocop -D
-bundle exec script/validate-html
+bundle exec script/validate-html.rb
 gem build jekyll-theme-cayman.gemspec
